cb96b5c7f976f518104db47282fb03973f746b28
Richárd Zloch <zloch.richard@verebelyszki.hu>
2020-09-22 12:58:24 +0200

    asd

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 30;
+            int x = 1;
+            int y = 1;
+            Console.WriteLine($"{x} {y} ");
+            for (int i = 0; i < n; i++)
+            {
+                int k = x + y;
+                Console.Write($"{k} ");
+                x = y;
+                y = x;
+            }
+            Console.ReadKey();
+        }
+    }
+}
